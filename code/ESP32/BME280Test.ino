1d60ba52930efca8a78bb2cd88fea5084b2697ac
Coconut Team Robotics <90797450+coconutrobotics@users.noreply.github.com>
2022-04-19 21:32:33 +0300

    Create BME280Test.ino

---
+++ code/ESP32/BME280Test.ino
@@ -0,0 +1,56 @@
+//Libraries
+#include <Wire.h>                       
+#include <Adafruit_BME280.h>            
+
+
+//Constants
+#define SEALEVELPRESSURE_HPA (1013.25)  //Pressure at sea level, for BME280
+
+//Variables
+bool result_bme;                        //Boolean for checking if BME280 works                 
+
+//Objects
+Adafruit_BME280 bme;                    
+
+void setup() {
+  Serial.begin(115200);   
+  result_bme = bme.begin();
+  if (result_bme == false) 
+  {
+    Serial.println("BME280 does not work! Check wiring!");
+  } else {
+    Serial.println("BME280 works fine!");  
+  }
+}
+
+void loop() {
+  if (Serial.available() > 0) {  
+    String command = Serial.readString();   
+    command.trim();                         
+    if (command == "getdata") {             
+      Serial.println("Preparing data");
+      if (result_bme == true) {
+        float temperature = bme.readTemperature();
+        float pressure = bme.readPressure()/100.0F;
+        float humidity = bme.readHumidity();
+        float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+        //Temperature
+        Serial.print("Temperature: ");                           
+        Serial.print(temperature);
+        Serial.println(" celcius");
+        //Pressure
+        Serial.print("Pressure: ");
+        Serial.print(pressure);
+        Serial.println(" hPa");
+        //Humidity
+        Serial.print("Humidity: ");
+        Serial.print(humidity);
+        Serial.println(" %");
+        //Altitude
+        Serial.print("Altitude: ");
+        Serial.print(altitude);
+        Serial.println(" m");
+      }
+    }
+  }
+}
